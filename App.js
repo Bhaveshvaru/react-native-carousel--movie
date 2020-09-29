f0e8318efa0dff2488f45a77bad559d8738169fe
BhaveshVaru <bhavesh.varu97@gmail.com>
2020-09-29 23:59:49 +0530

    added react native navigation

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+
+//screens
+
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>This is app </Text>
-    </View>
+   <NavigationContainer>
+       <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+   </NavigationContainer>
   );
 };
 
